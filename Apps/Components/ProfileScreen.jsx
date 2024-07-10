4aa4aa22d73c66934910cddc7a276f33bfd32266
navaneethkumar1 <141226088+navaneethkumar1@users.noreply.github.com>
2024-07-10 16:48:30 +0530

    tenth

--- Apps/Components/ProfileScreen.jsx
+++ Apps/Components/ProfileScreen.jsx
@@ -1,8 +1,8 @@
-import { View, Text, TouchableOpacity,StyleSheet } from 'react-native'
+import { View, Text, TouchableOpacity,StyleSheet,Image } from 'react-native'
 import React from 'react'
 import Colors from '../Utils/Colors'
 import { useNavigation } from '@react-navigation/native';
-import { useClerk } from '@clerk/clerk-expo';
+import { useClerk, useUser } from '@clerk/clerk-expo';
 export default function ProfileScreen() {
     const navigation = useNavigation();
     const { signOut } = useClerk();
@@ -10,10 +10,23 @@ export default function ProfileScreen() {
     const handleLogout = async () => {
       await signOut();
       navigation.replace('Login');
+     
+    
+    
     }
+      //display user info
+    const {user}=useUser();
+
   return (
     <View>
-      <TouchableOpacity onPress={handleLogout}style={styles.logout_btn}><Text style={{color:Colors.white}}>Logout</Text></TouchableOpacity>
+      <View style={styles.userinfo}>
+        <Image source={{ uri:user?.imageUrl}} style={{width:90,height:90,borderRadius:99}} />
+        <Text style={{fontSize:30,alignItems:'center'}}>{user.fullName}</Text>
+        <Text style={{fontSize:20}}>{user?.primaryEmailAddress.emailAddress}</Text>
+      </View>
+      <TouchableOpacity onPress={handleLogout}style={styles.logout_btn}>
+        <Text style={{color:Colors.white}}>Logout</Text>
+        </TouchableOpacity>
     </View>
   )
 }
@@ -24,6 +37,14 @@ padding:18,
 margin:30,
 borderRadius:10,
 alignItems:'center',
-marginTop:730
+marginTop:500
+},
+userinfo:{
+  marginTop:30,
+  marginLeft:10,
+  padding:10,
+  display:'flex',
+  flexDirection:'column',
+  alignItems:'center'
 }
 })
