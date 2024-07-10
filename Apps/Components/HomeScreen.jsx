8d0ee6337f8e340f2b29895fe675fc6209d1bd37
navaneethkumar1 <141226088+navaneethkumar1@users.noreply.github.com>
2024-07-10 15:42:02 +0530

    ningth

--- Apps/Components/HomeScreen.jsx
+++ Apps/Components/HomeScreen.jsx
@@ -1,9 +1,7 @@
 // 
 import { View, Text, StyleSheet } from 'react-native';
 import React, { useEffect, useState } from 'react';
-import { getCourseList } from '../Utils/GlobalApi';
 import Header from './Header';
-import CourseList from './CourseList';
 
 export default function HomeScreen() {
 
--- Apps/Components/ProfileScreen.jsx
+++ Apps/Components/ProfileScreen.jsx
@@ -1,16 +1,29 @@
 import { View, Text, TouchableOpacity,StyleSheet } from 'react-native'
 import React from 'react'
-
+import Colors from '../Utils/Colors'
+import { useNavigation } from '@react-navigation/native';
+import { useClerk } from '@clerk/clerk-expo';
 export default function ProfileScreen() {
+    const navigation = useNavigation();
+    const { signOut } = useClerk();
+  
+    const handleLogout = async () => {
+      await signOut();
+      navigation.replace('Login');
+    }
   return (
     <View>
-      <Text>ProfileScreen</Text>
-      <TouchableOpacity style={styles.logout_btn}></TouchableOpacity>
+      <TouchableOpacity onPress={handleLogout}style={styles.logout_btn}><Text style={{color:Colors.white}}>Logout</Text></TouchableOpacity>
     </View>
   )
 }
 const styles=StyleSheet.create({
 logout_btn:{
-
+backgroundColor:Colors.Primary,
+padding:18,
+margin:30,
+borderRadius:10,
+alignItems:'center',
+marginTop:730
 }
 })
